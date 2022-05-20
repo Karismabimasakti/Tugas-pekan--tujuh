e5fb71df8b11555fe71201cd0f23c5e4a695abd6
karisma bima sakti <102411101+Karismabimasakti@users.noreply.github.com>
2022-05-20 11:21:41 +0700

    Create dataset anggota keluarga.py

---
+++ dataset anggota keluarga.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+pizza = {'Nama' : [bima', 'johar', 'retno'],
+'Tinggi badan': [171, 175, 169],
+'Berat Badan' : [54, 73, 60],
+}
+pizza_df = pd.DataFrame(pizza)
+pizza_df
+print(pizza_df)
